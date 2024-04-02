e889acfdf54988f9db0632bf74b5fa3a336f1b08
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-04-02 22:00:21 +0800

    Delete luci-app-devcom directory

--- luci-app-devcom/luasrc/controller/devcom.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.devcom",package.seeall)
-function index()
-	
-	local page
-	
-	page = entry({"admin", "network", "DEVCOM"}, cbi("devcom"), _("设备备注"), 15)
-	page.dependent = true
-end
